d0bcd0309e4bb47b8613d7f7e9fd738f47a8ac8b
Joel Delight and Noah Yasskin <noah@>
2016-06-01 09:03:18 -0700

    add functionality of current site to repeat the phrase entered successfully.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
     $("#blanks form").submit(function(event) {
-      var person1Input = $("input#person1").val();
-      var person2Input = $("input#person2").val();
-      var animalInput = $("input#animal").val();
-      var exclamationInput = $("input#exclamation").val();
-      var verbInput = $("input#verb").val();
-      var nounInput = $("input#noun").val();
+      var repeat1Input = $("input#repeat1").val();
 
-      $(".person1").text(person1Input);
-      $(".person2").text(person2Input);
-      $(".animal").text(animalInput);
-      $(".exclamation").text(exclamationInput);
-      $(".verb").text(verbInput);
-      $(".noun").text(nounInput);
+      var += repeat1Input.toUpperCase();
 
-      $("#story").show();
+      $(".repeat1").text(repeat1Input);
+
+      $("#yell").show();
 
       event.preventDefault()
     });
